dbc02c7da54117330425aa403e6e8044547f7d77
Mukul Jangid <mukuljd91@gmail.com>
2026-08-15 05:52:04 +0530

    updating the resume_parser file which extracts text from resume pdf and docx

--- resume_parser.py
+++ resume_parser.py
@@ -5,11 +5,70 @@ import docx
 class ExtractionError(Exception):
     pass
 
+# extraction of text from pdf
 def extract_text_from_pdf(file_bytes:bytes) -> str:
-    pass   # extraction of text from pdf
+    text_parts = []
+    try:
+        with pdfplumber.open(io.BytesIO(file_bytes)) as pdf:
+            for page in pdf.pages:
+                page_text = page.extract_text()
 
+                if (page_text):
+                    text_parts.append(page_text)
+
+    except Exception as e:
+        raise ExtractionError(f"unable to read your pdf file: {e}")
+
+    full_text = '\n'.join(text_parts).strip()
+
+    if not full_text:
+        raise ExtractionError('unable to extract text from your resume, try putting pdf and docx based resume')
+
+    return full_text
+
+ # extraction of text from docx
 def extract_text_from_docx(file_bytes:bytes) -> str:
-    pass   # extraction of text from docx
+    try:
+        document = docx.Document(io.BytesIO(file_bytes))
+    except Exception as e:
+        raise ExtractionError(f'unable to read the document : {e}')
 
-def extract_text_from_resume(file_bytes:bytes) -> str:
-    pass  # here i will write code for checking file extension
+    parts = []
+
+    #paragraphs
+    for p in document.paragraphs:
+        if (p.text.strip()):
+            parts.append(p.text.strip())
+
+    #tabular data
+    for table in document.tables:
+        for row in table.rows:
+            for cell in row.cells:
+                if (cell.text.strip()):
+                    parts.append(cell.text.strip())
+
+    full_text = '\n'.join(parts).strip()
+
+    if not full_text:
+        raise ExtractionError('no extractable text from your docx file')
+
+    return full_text
+
+# here i will write code for checking file extension
+def extract_resume_text(uploaded_file) -> str:
+
+    if(uploaded_file is None):
+        raise ExtractionError('No file provided yet')
+
+    file_name = uploaded_file.name.lower()
+    file_bytes = uploaded_file.getvalue()
+
+    if not file_bytes:
+        raise ExtractionError('uploaded file is empty')
+
+    if (file_name.endswith('.pdf')):
+        extract_text_from_pdf(file_bytes)
+    elif(file_name.endswith('.docx')):
+        extract_text_from_docx(file_bytes)
+    else:
+        raise ExtractionError('unsupported file type, please upload pdf or docx file type')
